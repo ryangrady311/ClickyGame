997caa06ee9050cd44d0cb05d5003c37e8b30446
Ryan Grady <ryangrady250@gmail.com>
2019-06-08 21:58:52 -0700

    Updates

---
+++ precache-manifest.77673aa42e34bb54ba1b8fd2f411ec0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbafd11de092b40a9df65831c404951c",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "9e00cbcd1f69213b302e",
+    "url": "/ClickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ClickyGame/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9e00cbcd1f69213b302e",
+    "url": "/ClickyGame/static/js/main.d6957d25.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
