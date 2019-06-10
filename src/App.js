999e80edd65eb71ec069e9155d6ed94b7c529bf7
Ryan Grady <ryangrady250@gmail.com>
2019-06-09 19:00:11 -0700

    working shuffled JSON array

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 
+var shuffle = require('shuffle-array');
+ 
+// shuffle(collection);
+ 
+// console.log(collection); // returns [4, 3, 1, 5, 2]
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends
   };
 
+
+  
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
@@ -19,18 +27,17 @@ class App extends Component {
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+
+    
     return (
       <Wrapper>
         <Title>Clicky Game!</Title>
-        {this.state.friends.map(friend => (
+       
+        {shuffle(this.state.friends).map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
